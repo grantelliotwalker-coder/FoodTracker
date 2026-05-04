77a327a5defc41bf57bd210a90651656e5d1b33e
grantelliotwalker-coder <grantelliotwalker@gmail.com>
2026-05-04 13:08:10 +1000

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'food-tracker-v1';
+const ASSETS = [
+  './food-tracker.html',
+  './food-library.js',
+  'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300;400;500;700&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
